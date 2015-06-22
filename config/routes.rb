9a5d0f363a1e7943793bd81d0e56fb8e3a5ca180
Roope Salmi <rpsalmi@gmail.com>
2015-06-22 21:04:53 +0300

    Add index controller and root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  resources :logs
-
-  resources :events
-
-  resources :users
-
-  resources :items
+  
+  root 'index#index'
+  
+  resources :logs, :events, :users, :items
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
