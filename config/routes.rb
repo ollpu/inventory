77d9f9203520111b6a7359271d3a1701d7559e33
Roope Salmi <rpsalmi@gmail.com>
2015-06-24 14:46:06 +0300

    Add `search`-controller and a search field with some styles for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   root 'index#index'
-  
+  get 'search' => 'search#results'
   resources :logs, :events, :users, :items
 
   # The priority is based upon order of creation: first created -> highest priority.
