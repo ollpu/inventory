7d7b04f78fccacff594632abbac6c15f4c272cd8
Roope Salmi <rpsalmi@gmail.com>
2015-06-26 21:07:00 +0300

    Add items#new and some other items-methods without views yet

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,21 @@
 class ItemsController < ApplicationController
+  def index
+  end
+  
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    
+    @item.save
+    redirect_to item_path(@item)
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:type)
+  end
+  
 end
--- app/models/items/item.rb
+++ app/models/items/item.rb
@@ -3,6 +3,14 @@ require 'securerandom'
 class Item < ActiveRecord::Base
   before_save :generate_uid, on: :new
   
+  TYPES = [
+    ['Cable', 'Cable'],
+    ['Device', 'Device'],
+    ['Person', 'Person'],
+  ]
+  
+  
+  
   def generate_uid
     self.uid = SecureRandom.uuid
   end
