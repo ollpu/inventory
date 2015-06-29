483bc49e4e3f4715dc69a8be412863dc51cb8843
Roope Salmi <rpsalmi@gmail.com>
2015-06-29 16:53:58 +0300

    :white_check_mark: Write tests for Item

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Rails.application.routes.url_helpers
+  
+  test 'generate_uid should return uuid' do
+    item = Item.new
+    item.generate_uid
+    
+    assert_match /\h{8}-\h{4}-\h{4}-\h{4}-\h{12}/, item.uid, "generate_uid did not generate a valid UUID"
+  end
+  
+  test 'uid should be generated before save' do
+    item = Item.new
+    item.type = "Cable"
+    
+    item.save
+    assert_not_empty item.uid, "Item was saved without uid"
+  end
+  
+  test 'type should be present' do
+    item = Item.new
+    assert_raises ActiveRecord::RecordInvalid do
+      item.save!
+    end
+    
+    assert_nil item.uid, "uid was generated for invalid item"
+    
+    assert_raises ActionController::UrlGenerationError do
+      item_path item
+      item_url item
+    end
+  end
+  
+  test 'get_type_name function' do
+    item = Item.new
+    item.type = "Device"
+    
+    assert_equal I18n.t(:device, scope: [:activerecord, :models]),
+      item.get_type_name,
+      "get_type_name did not return right translation"
+    
+  end
+  
+  test 'to_param function' do
+    item = Item.new
+    item.generate_uid
+    
+    assert_equal item.uid, item.to_param, "to_param did not return actual uid"
+  end
+    
 end
