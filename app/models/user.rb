0dec6a47a5d2842f1ded83c50022996dd9a76028
Roope Salmi <rpsalmi@gmail.com>
2015-07-11 22:54:17 +0300

    Start working on Users controller and User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,27 @@
 class User < ActiveRecord::Base
   
+  attr_accessor :password, :password_confirmation
+  before_save :encypt_password
+  
+  validates :password_confirmation,
+    presence: true,
+    on: :new
+  validates :password,
+    presence: true,
+    confirmation: { :on => :create },
+    on: :new
+  validates :email,
+    presence: true,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },
+    uniqueness: { case_sensitive: false }
+  
+  def encypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+  
   protected
     def after_create
       # Expire main trivia (includes User.count)
