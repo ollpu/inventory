113571f83e9f1a66925d8d2468ac9d9eb28c2b73
Roope Salmi <rpsalmi@gmail.com>
2015-08-23 17:44:34 +0300

    Finalize users#show password changing fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,38 @@ class UsersController < ApplicationController
   end
   
   def update
-    @user = User.new(user_params)
-    # A new password is 
-    user.save!
+    @user = User.find(params[:id])
+    authorize @user
+    unless @user.update user_update_params(@user) # update record
+      # if updating failed (is user invalid?)
+      unless @user.valid?
+        # User invalid, render form again and display errors
+        render 'show'
+      else
+        # Unexpected situation, display error message
+        @user.update! user_update_params(@user)
+      end
+    else
+      # updating successful
+      redirect_to user_path(@user), notice: t(:modification_success, scope: [:users, :show])
+    end
   end
   
   private
   
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+  
+  def user_update_params user_to_update
+    unless current_user.admin? and not current_user == user_to_update
+      # User is not an admin or is modifying self
+      params.require(:user).permit(:email, :password, :password_confirmation,
+        :old_password)
+    else
+      # User is an admin (and not modifying self)
+      params.require(:user).permit(:email, :password_admin, :password_confirmation)
+    end
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,54 @@
 class User < ActiveRecord::Base
   
-  attr_accessor :password, :password_confirmation
-  before_save :encypt_password
+  attr_accessor :password_confirmation, :old_password
+  attr_reader :password
+  def password= string
+    @password = string
+    @prev_password_hash = password_hash
+    @prev_password_salt = password_salt
+    encrypt_password
+  end
+  
+  # password accessors for admin (sets @password_admin to true)
+  def password_admin= string
+    @password_admin = true
+    password = string
+  end
+  def password_admin; password; end
+  
   
   validates :password_confirmation,
     presence: true,
-    on: :new
+    if: "password.present?"
   validates :password,
     presence: true,
     confirmation: true,
-    on: :new
+    length: { minimum: 5 },
+    if: "password.present?"
+  
+  validate :old_password_matching
+  
+  def old_password_matching
+    if password.present? and not @password_admin and @prev_password_hash.present?
+      old_password_hash = BCrypt::Engine.hash_secret(old_password, @prev_password_salt)
+      unless old_password_hash == @prev_password_hash
+        errors[:old_password] << I18n.t(:old_password_no_match, scope: [:errors])
+      end
+    end
+  end
+  
+  validates :password_hash, presence: true
+  validates :password_salt, presence: true
+  
   validates :email,
     presence: true,
-    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
     uniqueness: { case_sensitive: false }
   validates :privileges,
     presence: true,
     inclusion: { in: 0..2 }
   
-  def encypt_password
+  def encrypt_password
     if password.present?
       self.password_salt = BCrypt::Engine.generate_salt
       self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
